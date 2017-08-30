e3e1f8df47c85b271528d3339f71c62244ca7b37
Calvin0630 <cg7057705@gmail.com>
2017-08-30 19:52:12 -0400

    jujuju

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
     Rigidbody rBody;
     public float walkSpeed;
     Vector3 walkDir;
+    //an angle representing the angle that the camera is turned on the xz plane
+    float cameraAngle;
 	// Use this for initialization
 	void Start () {
         rBody = this.GetComponent<Rigidbody>();
@@ -14,7 +16,10 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         walkDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        print(ThirdPersonCamera.cameraDir);
+        print("CameraDir: " + ThirdPersonCamera.cameraDir);
+        //camera angle = cos(^-1) z/hypoteneuse
+        cameraAngle = 
+        print("Camera Angle: " + cameraAngle);
         rBody.AddForce(walkSpeed * walkDir);
         if (Input.GetButton("Fire1")) print("Hello");
 	}
