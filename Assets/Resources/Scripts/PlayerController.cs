979a7272d9fc12d9ec7296d4a90bdfc4c8752d1b
Calvin0630 <cg7057705@gmail.com>
2017-08-30 18:31:04 -0400

    first version
    walking needs work

---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody rBody;
+    public float walkSpeed;
+    Vector3 walkDir;
+	// Use this for initialization
+	void Start () {
+        rBody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        walkDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        print(ThirdPersonCamera.cameraDir);
+        rBody.AddForce(walkSpeed * walkDir);
+        if (Input.GetButton("Fire1")) print("Hello");
+	}
+}
---
+++ Assets/Resources/Scripts/WorldGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  
+
+public class WorldGenerator : MonoBehaviour {
+
+    GameObject groundPrefab;
+    GameObject[] groundArray;
+    GameObject ground;
+    GameObject buildingPrefab;
+    GameObject[] buildingArray;
+    GameObject buildings;
+	// Use this for initialization
+	void Start () {
+        groundPrefab = Resources.Load("Prefab/Ground") as GameObject;
+        buildingPrefab = Resources.Load("Prefab/Building") as GameObject;
+        ground = new GameObject();
+        ground.name = "Ground";
+        ground.transform.SetParent(this.transform);
+        buildings = new GameObject();
+        buildings.name = "Buildings";
+        buildings.transform.SetParent(this.transform);
+        SpawnGround();
+        SpawnBuildings();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnGround() {
+        groundArray = new GameObject[100 * 100];
+        for (int i=0;i<100;i++) {
+            for (int j = 0;j<100;j++) {
+                Vector3 pos = new Vector3(495 - i*10, 0, 495 - j*10);
+                groundArray[i * 100 + j] = Instantiate(groundPrefab, pos, Quaternion.identity);
+                groundArray[i * 100 + j].name = "Ground[" + i + ", " + j + "]";
+                groundArray[i * 100 + j].transform.SetParent(ground.transform);
+            }
+        }
+    }
+
+    public void SpawnBuildings() {
+        buildingArray = new GameObject[50*50];
+        for (int i=0;i<50;i++) {
+            for (int j=0;j<50;j++) {
+                Vector3 pos = new Vector3(495 - (2*i*10), 10, 495 - (2*j*10));
+                buildingArray[i * 50 + j] = Instantiate(buildingPrefab, pos, Quaternion.identity);
+                buildingArray[i * 50 + j].name = "Building[" + i + ", " + j + "]";
+                buildingArray[i * 50 + j].transform.SetParent(buildings.transform);
+            }
+        }
+    }
+}
